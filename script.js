9fb10ed5b3602e32f98aaafb3c53f673853d8ce1
gaginho <23i03g@mail.ru>
2015-07-26 01:22:38 +0300

    killing process added

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ var url = require('url');
 var queryProcessor = require('./test');
 var sender = require('./requestSender');
 
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+
 var qs = require('querystring');
 
 var server = new http.Server();
 var serverName = "FrontendServer";
 //console.log(queryProcessor.getOwnPropertyNames());
+//exec("exit");
+
+/*var spawn = require('child_process').spawn,
+    grep  = spawn('grep', ['ssh']);
+
+grep.on('close', function (code, signal) {
+  console.log('child process terminated due to receipt of signal '+signal);
+});
+
+// send SIGHUP to process
+grep.kill('SIGHUP');*/
 
 var user1 = qs.stringify({
       login: 'Dinesh',
@@ -49,44 +64,59 @@ server.on('request', function (req,res) {
             console.log("Url= " + command);
             console.log("Key =" + post['login']);
             console.log("Key =" + post['job']);
-
+            
             Respond(command, post, res);
-
+            //KillServer();
 
 	
             // use post['blah'], etc.
         });
 });
+
+function KillServer(){
+	server.close();
+	//exec("killall node");
+	function puts(error, stdout, stderr) { sys.puts(stdout) }
+	exec("killall gnome-terminal", puts);
+	//exec("killall node", puts);
+	//server.stop();
+	console.log("Stoping Server");
+	/*exec("exit");*/
+}
 function Respond(command, data, res){///res=response
 	res.setHeader('Content-Type', 'application/json');
 	res.write(JSON.stringify(cars));
         switch(command){
-		case '/start':
-			//sender.sendRequest("register", options, user1,   );
-			console.log("Sending back" + JSON.stringify(post));
+        	case '/stop':
+        		res.end("Finish");
+        		KillServer();
+        	break;
+			case '/start':
+				//sender.sendRequest("register", options, user1,   );
+				console.log("Sending back" + JSON.stringify(post));
 
-			//queryProcessor.start(res, JSON.stringify(post))
-			//res.end("Hola");
-		break;
-		case '/register':
-			console.log("Port=" + queryProcessor.getPort('AccountServer'));
-			sender.sendRequest("register", user1, '127.0.0.1', queryProcessor.getPort('AccountServer'), 
-				function (res1) {
-				    res1.setEncoding('utf8');
-				    res1.on('data', function (chunk) {
-						console.log("body: " + chunk);
-						///analyse and return answer to client-bot
-						res.end("THX for register");
-				    });
+				//queryProcessor.start(res, JSON.stringify(post))
+				//res.end("Hola");
+			break;
+			case '/register':
+				console.log("Port=" + queryProcessor.getPort('AccountServer'));
+				sender.sendRequest("register", user1, '127.0.0.1', queryProcessor.getPort('AccountServer'), 
+					function (res1) {
+					    res1.setEncoding('utf8');
+					    res1.on('data', function (chunk) {
+							console.log("body: " + chunk);
+							///analyse and return answer to client-bot
+							res.end("THX for register");
+					    });
 
-				    /*req.on('error', function(e) {
-					console.log('problem with request: ' + e.message);
-					});*/
-				}
-			);
-		break;
-		default:
-			res.end("Chiao");
-			break;	
+					    /*req.on('error', function(e) {
+						console.log('problem with request: ' + e.message);
+						});*/
+					}
+				);
+			break;
+			default:
+				res.end("Chiao");
+			break;
 		}
 }
--- startScript.sh
+++ startScript.sh
@@ -1,5 +1,7 @@
 echo "START SH SCRIPT WORKS!!"
 gnome-terminal -x sh -c 'node /home/gaginho/project/NODE/script.js; exec bash'
 echo 'SCRIPT script.js WORKS!!'
-gnome-terminal -x sh -c 'node /home/gaginho/project/NODE/Admin.js; exec bash'
-echo 'SCRIPT Admin.js WORKS!!'
+gnome-terminal -x sh -c 'node /home/gaginho/project/NODE/AccountServer.js; exec bash'
+echo 'SCRIPT AccountServer.js WORKS!!'
+gnome-terminal -x sh -c 'node /home/gaginho/project/NODE/client-bot.js; exec bash'
+echo 'SCRIPT client-bot.js WORKS!!'
