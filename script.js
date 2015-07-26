bf0b1ce262242a004a7c5b870bf2373bdeccf3e9
gaginho <23i03g@mail.ru>
2015-07-26 16:40:15 +0300

    Making and abstract layer for Frontend. Needs a little refactoring

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var http = require('http');
 var url = require('url');
 var queryProcessor = require('./test');
-var sender = require('./requestSender');
+//var sender = require('./requestSender');
 
 var sys = require('sys')
 var exec = require('child_process').exec;
-
+this.SetServer = SetServer;
 
 var qs = require('querystring');
 
 var server = new http.Server();
-var serverName = "FrontendServer";
+//var serverName = "FrontendServer";
 //console.log(queryProcessor.getOwnPropertyNames());
 //exec("exit");
 
@@ -24,57 +24,51 @@ grep.on('close', function (code, signal) {
 // send SIGHUP to process
 grep.kill('SIGHUP');*/
 
-var user1 = qs.stringify({
-      login: 'Dinesh',
-      password: 'Kumar',
-	job   : [ 'language', 'PHP' ]
-    });
+
 
 process.argv.forEach(function (val, index, array) {
   console.log(index + ': ' + val);
 });
 
-console.log("More changes");
-
-var port = queryProcessor.getPort(serverName);
-console.log("Port " + port);
-console.log(queryProcessor.area(5));
-server.listen(port, '127.0.0.1');
 var cars = ["Saab", "Volvo", "BMW"]; cars.push("LADADADADADA");
 
-server.on('request', function (req,res) {
-	console.log("------------------------");
-	//////res.write(JSON.stringify({ a: 1 }));
+function SetServer(serverName, serverUrl, functionArray) {
+	console.log("Starting " + serverName);
+	var port = queryProcessor.getPort(serverName);
+	console.log("Port " + port);
+	console.log(queryProcessor.area(5));
+	server.listen(port, serverUrl);//'127.0.0.1'
+	funcArray = functionArray;
+	server.on('request', function (req,res) {
+		console.log("------------------------");
+		//////res.write(JSON.stringify({ a: 1 }));
 
-	var urlParsed = url.parse(req.url);
-	console.log(urlParsed.query);
+		var urlParsed = url.parse(req.url);
+		console.log(urlParsed.query);
 
-	var command = urlParsed.pathname;
-	var body = '';
-        req.on('data', function (data) {
-            body += data;
-            console.log("incoming data: " + data + " \n");
-            // Too much POST data, kill the connection!
-            if (body.length > 1e6){
-                req.connection.destroy();
-            }
-        });
-        req.on('end', function () {
-            var post = qs.parse(body);
-            console.log(post);
-            console.log("stringifying:" + JSON.stringify(post));
-            console.log("Url= " + command);
-            console.log("Key =" + post['login']);
-            console.log("Key =" + post['job']);
-            
-            Respond(command, post, res);
-            //KillServer();
-
-	
-            // use post['blah'], etc.
-        });
-});
-
+		var command = urlParsed.pathname;
+		var body = '';
+	        req.on('data', function (data) {
+	            body += data;
+	            console.log("incoming data: " + data + " \n");
+	            // Too much POST data, kill the connection!
+	            if (body.length > 1e6){
+	                req.connection.destroy();
+	            }
+	        });
+	        req.on('end', function () {
+	            var post = qs.parse(body);
+	            console.log(post);
+	            console.log("stringifying:" + JSON.stringify(post));
+	            /*console.log("Url= " + command);
+	            console.log("Key =" + post['login']);
+	            console.log("Key =" + post['job']);*/
+	            
+	            Respond(command, post, res);
+	            //KillServer();
+	        });
+	});
+}
 function KillServer(){
 	server.close();
 	//exec("killall node");
@@ -85,12 +79,22 @@ function KillServer(){
 	console.log("Stoping Server");
 	/*exec("exit");*/
 }
-var funcArray = {'/stop' : AnswerAndKill};//["/stop"]
+function TryToStopServer (data, res){
+	console.log("Checking Sender.... Sender is valid! (TODO FIX IT!!! do a proper Checking) ");
+	AnswerAndKill(data, res);
+}
+function AnswerAndKill(data, res) {
+	res.end("Finish");
+	KillServer();
+}
+var funcArray = {};
+
 //funcArray[1] = {'/register' : RegisterUser};
-funcArray["/register"] = RegisterUser;
+//550999 
 
 
-function RegisterUser(data, res){
+
+/*function RegisterUser(data, res){
 	console.log("Port=" + queryProcessor.getPort('AccountServer'));
 	sender.sendRequest("register", user1, '127.0.0.1', queryProcessor.getPort('AccountServer'), 
 		function (res1) {
@@ -101,39 +105,48 @@ function RegisterUser(data, res){
 				res.end("THX for register");
 		    });
 
-		    /*req.on('error', function(e) {
-			console.log('problem with request: ' + e.message);
-			});*/
+		    //req.on('error', function(e) {
+			//console.log('problem with request: ' + e.message);
+			//});
 		}
 	);
-}
-function AnswerAndKill(data, res) {
-	res.end("Finish");
-	KillServer();
-}
+}*/
+
+
+
 
 function Respond(command, data, res){///res=response
 	res.setHeader('Content-Type', 'application/json');
 	res.write(JSON.stringify(cars));
-        switch(command){
-        	case '/stop':
-        		//funcArray[command](data, res);
-        		funcArray[command](data, res);
-        		/*res.end("Finish");
-        		KillServer();*/
-        	break;
-			case '/start':
-				//sender.sendRequest("register", options, user1,   );
-				console.log("Sending back" + JSON.stringify(post));
-
-				//queryProcessor.start(res, JSON.stringify(post))
-				//res.end("Hola");
-			break;
-			case '/register':
-				funcArray[command](data, res);
-			break;
-			default:
-				res.end("Chiao");
-			break;
+	if (command == '/stop') { TryToStopServer(data, res); }
+	else{
+		try{
+			console.log("Trying to execute: " + command);
+			funcArray[command](data,res);
 		}
+		catch(error){
+			console.log("funcArray execution Exception: " + error);
+			res.end("Chiao");
+		}
+	}
+    /*switch(command){
+    	case '/stop':
+    		//funcArray[command](data, res);
+    		funcArray[command](data, res);
+    		//res.end("Finish"); KillServer();
+    	break;
+		case '/start':
+			//sender.sendRequest("register", options, user1,   );
+			console.log("Sending back" + JSON.stringify(post));
+
+			//queryProcessor.start(res, JSON.stringify(post))
+			//res.end("Hola");
+		break;
+		case '/register':
+			funcArray[command](data, res);
+		break;
+		default:
+			res.end("Chiao");
+		break;
+	}*/
 }
--- startScript.sh
+++ startScript.sh
@@ -1,6 +1,6 @@
 echo "START SH SCRIPT WORKS!!"
-gnome-terminal -x sh -c 'node /home/gaginho/project/NODE/script.js; exec bash'
-echo 'SCRIPT script.js WORKS!!'
+gnome-terminal -x sh -c 'node /home/gaginho/project/NODE/FrontendServer.js; exec bash'
+echo 'SCRIPT FrontendServer.js WORKS!!'
 gnome-terminal -x sh -c 'node /home/gaginho/project/NODE/AccountServer.js; exec bash'
 echo 'SCRIPT AccountServer.js WORKS!!'
 gnome-terminal -x sh -c 'node /home/gaginho/project/NODE/client-bot.js; exec bash'
