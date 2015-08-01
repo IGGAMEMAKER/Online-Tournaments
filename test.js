27eb49bcffb7fe0948667701237ed4572c4eaf8b
gaginho <23i03g@mail.ru>
2015-08-01 12:43:36 +0300

    A lot of modifications API v.0.2

--- test.js
+++ test.js
@@ -8,17 +8,30 @@ var serverList = {};
 
 serverList['FrontendServer'] = 5000;
 serverList['TournamentServer'] = 5001;
-serverList['GameServer'] = 5002;
+serverList['TournamentManager'] = 5002;
 serverList['AccountServer'] = 5003;
 serverList['BalanceServer'] = 5004;
 serverList['AdminServer'] = 5005;
 serverList['MoneyServer'] = 5006;
 serverList['DBServer'] = 5007;
-console.log(serverList['DBServer']);
 
+serverList['GameFrontendServer'] = 5008;
+serverList['GameServer'] = 5009;
+//console.log(serverList['DBServer']);
+
+var gameNameIDList = {};
+
+gameNameIDList['1'] = 5000;
+gameNameIDList['2'] = 5001;
+gameNameIDList['3'] = 5002;
+gameNameIDList['4'] = 5003;
 
 this.getPort = function(r){
 	return serverList[r];
+}
+
+this.getGamePort = function(r){
+	return gameNameIDList[r];
 }
 
 this.area = function ( r ) {
