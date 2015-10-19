67ed2c9e47fc9b0c519158303a82e8e658ddebff
gaginho <23i03g@mail.ru>
2015-10-20 00:54:16 +0300

    added shell script to run apps with PM2

--- pmStart.sh
+++ pmStart.sh
@@ -1,1 +1,11 @@
-sudo pm2 start site.js --name site
+pm2 delete all
+pm2 start site.js --name site -i 1
+pm2 start DBServer.js --name DB
+pm2 start TournamentServer.js --name TS
+pm2 start BalanceServer.js --name BS
+pm2 start MoneyServer.js --name MS
+pm2 start GameFrontendServer.js --name GFS
+pm2 start Servers/PingPongServer.js --name PP
+pm2 start Servers/QuestionServer.js --name QS
+pm2 list
+pm2 monit
