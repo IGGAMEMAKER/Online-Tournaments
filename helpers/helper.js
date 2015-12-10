f6272c28bbed7b15cd46bc6533868f2fa2d19b24
gaginho <23i03g@mail.ru>
2015-12-11 02:22:01 +0300

    added helpers and gifts

---
+++ helpers/helper.js
@@ -0,0 +1,38 @@
+
+
+function log(msg){ console.log(msg); }
+
+function now(){ return new Date(); }
+
+
+function catcher(err){
+	log('catched error!');
+	if (err){
+		log(err.stack||err);
+	} else {
+		log('null error');
+	}
+}
+
+function updated(count){
+	//console.log('Updated : ' + JSON.stringify(count), STREAM_USERS );
+	return count.n>0;
+}
+
+function p_printer (obj) {
+	return new Promise(function(resolve, reject){
+		printer(obj);
+		return resolve(obj);
+	})
+}
+
+function printer(obj) { 
+	log('obj:'); 
+	log(obj); 
+}
+
+this.log = log;
+this.now = now;
+this.catcher = catcher;
+this.updated = updated;
+this.p_printer = p_printer;
---
+++ models/gifts.js
@@ -0,0 +1,63 @@
+var Promise = require('bluebird');
+
+var configs = require('../configs');
+var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://'+configs.db+'/test');
+
+var helper = require('../helpers/helper');
+var log = helper.log;
+
+var Gift = mongoose.model('Gift', { 
+	name: String, photoURL: String, 
+	description: String, URL: String, 
+	price: Number, sended:Object, 
+	date:Date 
+});
+
+function all(query){
+	return new Promise(function(resolve, reject){
+		Gift.find(query||{}, function (err, gifts){
+			if (err) return reject(err);
+
+			return resolve(gifts||null);
+		});
+	})
+}
+
+function getByID(giftID){
+	return new Promise(function(resolve, reject){
+		Gift.findOne({ _id : giftID}, function (err, gift){
+			if (err) return reject(err);
+
+			return resolve(gift||null);
+		});
+	});
+}
+
+function add(data){
+	return new Promise(function(resolve, reject){
+		log('trying to add gift '+ JSON.stringify(data)+ ' Gifts');
+
+		if (data){
+			gift = new Gift(data);
+			gift.save(function (err){
+				if (err) return reject(err);
+
+				log('Added gift ' + JSON.stringify(data) + ' Gifts');
+				return resolve(OK);
+			})
+		} else {
+			log('No addition. Gift is null' + ' Gifts');
+			return reject(Fail);
+		}
+	});
+
+}
+
+log('asdddd');
+getByID('5622b320ecdf83f91ef09036')
+.then(helper.p_printer)
+.catch(helper.catcher);
+
+//function log(msg){ console.log(msg); }
