6fbd298b17668158f3ea2a640216192374cf9101
gaginho <23i03g@mail.ru>
2015-12-13 00:06:20 +0300

    second changes

--- page.js
+++ page.js
@@ -17,34 +17,36 @@ document.write("			<\/div>");*/
 
 function print_tournament(t, imageUrl){
 	var ID=t.ID||115;
-	t.goNext = [2];
+	//t.goNext = [2];
 	var registered = 1;
-	var strVar="";
-	var winnersPlaces = 1;
 	var gameNameID = t.gameNameID;
 
 	var prizes = t.Prizes[0];
+	
 	var maxPlayers = t.goNext[0];
+	var winnersPlaces = t.goNext[1];
 
 	var buyIn = t.buyIn || 0;
 
-	strVar += "<div class=\"col-sm-3\">";
+	var strVar="";
+	strVar += "<div class=\"col-sm-3\"><center>";
 	strVar += "				<h4 class=\"mg-md text-center\"";
 	strVar += get_gameName(ID, gameNameID);
-	strVar += "				<\/h4><img src=\""+imageUrl+"\" class=\"img-rd-lg\" width=\"210\" height=\"150\" \/>";
+	strVar += "				<\/h4>"
+	strVar += "<img src=\""+imageUrl+"\" class=\"img-rd-lg\" width=\"210\" height=\"150\" \/>";
 
 	strVar += "				<h3 class=\"mg-md\">";
 	strVar += get_price_word(buyIn);
 	strVar += "				<\/h3>";
 
-	strVar += get_prizes_word(ID, prizes);
-	strVar += "				<p>";
+	strVar += drawPrizes(ID, prizes);
+	strVar += "				<h4>";
 //	strVar += "					Playres:<br \/>Winers:<br \/>Time:<br \/>Questions:";
-	strVar += get_players_word(ID, maxPlayers, registered);
 	strVar += get_winners_word(ID, winnersPlaces);
-	strVar += "				<\/p>";
-	strVar += "				<a href=\"index.html\" class=\"btn btn-lg btn-primary\" style=\"width:60%; border-radius:0;\">Play<\/a>";
-	strVar += "			<\/div>";
+	strVar += get_players_word(ID, maxPlayers, registered);
+	strVar += "				<\/h4>";
+	strVar += "				<a href=\"index.html\" class=\"btn btn-lg btn-primary\" style=\"width:60%; border-radius:16px; \"> Играть <\/a>";//#2780e3
+	strVar += "			<\/center><\/div>";
 	
 	return strVar;
 }
@@ -54,7 +56,7 @@ var enter="<br ";
 
 
 function get_price_word(buyIn){
-	if (buyIn>0) return 'Стоимость: ' + buyIn;
+	if (buyIn>0) return 'Стоимость: ' + buyIn+'$';
 	return 'Бесплатно';
 }
 
@@ -65,6 +67,10 @@ function get_players_word(ID, max, registered){
 
 function get_gameName(ID, gameNameID){
 	var gname='Questions';
+	switch(gameNameID){
+		case 1: gname = 'Questions'; break;
+		case 2: gname = 'Ping pong'; break;
+	}
 	var word = "id=\"gname-"+ID+"\">"+gname;//"</p>";
 	return word;
 }
@@ -74,8 +80,14 @@ function get_winners_word(ID, winnersPlaces){
 	return word;
 }
 
+
 function get_prizes_word(ID, prizes){
-	var word = "<h4 id=\"wnrs-"+ID+"\"> Выигрыш: "+prizes +"</h4>";
-	return word;
+	/*var word = "<h4 id=\"wnrs-"+ID+"\"> Выигрыш: "+prizes +"</h4>";
+	return word;*/
+	return "Приз: "+prizes;
 }
 
+function drawPrizes(ID, prizes){
+	return "<h4 id=\"wnrs-"+ID+"\">"+get_prizes_word(ID,prizes)+"</h4>";
+	//return get_prizes_word(ID,prizes);
+}
