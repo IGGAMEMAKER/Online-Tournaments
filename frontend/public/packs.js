227c0c85bf2c7e3977be348691f1c6f626659634
IGGAMEMAKER <23i03g@mail.ru>
2016-03-25 03:30:46 +0300

    added images. updated players

--- frontend/public/packs.js
+++ frontend/public/packs.js
@@ -13,6 +13,20 @@ function drawCards(cards){
 
 function drawCard(card){
 	document.write(JSON.stringify(card))
+/*
+	<img border="0" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt=""">
+  <img class="cornerimage" border="0" src="http://www.gravatar.com/avatar/" alt="">
+*/
+	// $(cardField).append('<img border="0" style="background-color:blue" src="/img/topics/realmadrid/'+card.photoURL+'">')
 
-	$(cardField).append('<img src="/img/topics/realmadrid/'+card.photoURL+'">')
+	// $(cardField).append('<img border="0" style="background-color:blue" src="http://cdn.pcwallart.com/images/gold-texture-wallpaper-1.jpg">')
+	// $(cardField).append('<img class="cornerimage" border="0" src="/img/topics/realmadrid/'+card.photoURL+'">')
+	// $(cardField).append('<div>')
+	var backgroundImage = '\'http://cdn.pcwallart.com/images/gold-texture-wallpaper-1.jpg\''
+	// var backgroundImage = '\'img/cardLayers/gold.jpg\''
+	$(cardField).append('<img border="0" style="background-image:url('+backgroundImage+')" src="/img/topics/realmadrid/'+card.photoURL+'">')
+	
+	// $(cardField).append('<img border="0" style="background-color:blue" src="/img/topics/realmadrid/'+card.photoURL+'">')
+	// $(cardField).append('<img class="cornerimage" border="0" src="http://cdn.pcwallart.com/images/gold-texture-wallpaper-1.jpg" />')
+	// $(cardField).append('<img class="cornerimage" border="0" src="http://www.gravatar.com/avatar/" />')
 }
