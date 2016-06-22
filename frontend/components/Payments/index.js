0580e5837050ab91d805a52359eda695e9b91e5d
IGGAMEMAKER <23i03g@mail.ru>
2016-06-22 11:13:06 +0300

    need payment icons

--- frontend/components/Payments/index.js
+++ frontend/components/Payments/index.js
@@ -10,7 +10,9 @@ type PropsType = {
 };
 
 export default class Payment extends Component {
-  state = { };
+  state = {
+    chosen: 0,
+  };
 
   componentWillMount() {
     // this.loadData();
@@ -54,39 +56,105 @@ export default class Payment extends Component {
       </form>
     );
   };
+
+  choosePaymentType = (id) => {
+    return () => {
+      console.log('chosen', id);
+      this.setState({
+        chosen: id,
+      });
+    };
+  };
+
+  getPaymentImage = (chosen, alt, src, width, height) => {
+    return (
+      <img
+        src={src}
+        alt={alt}
+        onClick={this.choosePaymentType(chosen)}
+        width={width}
+        height={height}
+        className="light-blue-big"
+        style="margin-right: 15px;"
+      />
+    );
+  };
+
+  drawChosenPaymentForm = (chosen, ammount) => {
+
+    // const qiwi = '1) QIWI: +79648847260';
+    // const mobile = '2) С мобильного';
+    // const yandexMoney = '3) Яндекс.Деньги';
+    // const bankCard = '4) Банковская карточка';
+
+    const qiwi = 'QIWI: +79648847260';
+    const mobile = 'С мобильного';
+    const yandexMoney = 'Яндекс.Деньги';
+    const bankCard = 'Банковская карточка';
+
+    if (chosen === 1) {
+      return (
+        <div>
+          <h2 className="page">{qiwi}</h2>
+          <p className="page"> Укажите ваш логин ({login}) в комментарии к платежу </p>
+          <a
+            href="https://qiwi.ru"
+            target="_blank"
+            className="btn btn-lg btn-primary"
+          >Оплатить {ammount} руб</a>
+        </div>
+      );
+    }
+
+    if (chosen === 2) {
+      return (
+        <div>
+          <h2 className="page">{mobile}</h2>
+          <p className="page">НЕ УДАЛЯЙТЕ СМС до зачисления средств</p>
+          {this.payform(ammount, 'MC')}
+        </div>
+      );
+    }
+
+    if (chosen === 3) {
+      return (
+        <div>
+          <h2 className="page">{yandexMoney}</h2>
+          {this.payform(ammount, 'PC')}
+        </div>
+      );
+    }
+
+    if (chosen === 4) {
+      return (
+        <div>
+          <h2 className="page">{bankCard}</h2>
+          {this.payform(ammount, 'AC')}
+        </div>
+      );
+    }
+
+    return '';
+  };
   // <h2 className="page">Вы собираетесь пополнить счёт на {props.ammount} руб</h2>
-  render(props: PropsType) {
-    const qiwi = '1) QIWI: +79648847260';
-    const mobile = '2) С мобильного';
-    const yandexMoney = '3) Яндекс.Деньги';
-    const bankCard = '4) Банковская карточка';
-  return (
+  render(props: PropsType, state) {
+    const paymentForm = this.drawChosenPaymentForm(state.chosen, props.ammount);
+    const width = 50;
+    const height = 50;
+    return (
       <div>
         <h1 className="page">Пополнение счёта ({props.ammount} РУБ)</h1>
-        <h2 className="page">Способы оплаты: </h2>
-        <h2 className="page">{qiwi}</h2>
-        <p className="page"> Укажите ваш логин ({login}) в комментарии к платежу </p>
-        <a
-          href="https://qiwi.ru"
-          target="_blank"
-          className="btn btn-lg btn-primary"
-        >Оплатить {props.ammount} руб</a>
-
-        <hr colour="white" width="60%" align="center" />
-        <h2 className="page">{mobile}</h2>
-        <p className="page">НЕ УДАЛЯЙТЕ СМС до зачисления средств</p>
-        {this.payform(100, 'MC')}
-
-        <hr colour="white" width="60%" align="center" />
-        <h2 className="page">{yandexMoney}</h2>
-        {this.payform(100, 'PC')}
-
-        <hr colour="white" width="60%" align="center" />
-        <h2 className="page">{bankCard}</h2>
-        {this.payform(100, 'AC')}
-
-        <hr colour="white" width="60%" align="center" />
-        <br />
+        <h2
+          className="page"
+          style={{
+            display: state.chosen ? 'none' : 'block',
+          }}
+        >Выберите способ оплаты</h2>
+        {this.getPaymentImage(1, 'QIWI', '/img/qiwi.jpeg', width, height)}
+        {this.getPaymentImage(2, 'Оплата с мобильного', '/img/mobile.jpeg', width, height)}
+        {this.getPaymentImage(3, 'Яндекс.Деньги', '/img/yandex.png', width, height)}
+        {this.getPaymentImage(4, 'Оплата картой', '/img/qiwi.jpeg', width, height)}
+        {paymentForm}
         <br />
         <p className="white text-center">
           Если у вас возникли какие-то трудности, напишите в
