c56840751bcb10add5a66ce935eb234a056ea6c7
IGGAMEMAKER <23i03g@mail.ru>
2016-07-17 02:23:03 +0300

    more fixes to profile

--- frontend/components/Profile.js
+++ frontend/components/Profile.js
@@ -202,15 +202,38 @@ export default class Profile extends Component {
       </div>
     );
 
+    const depositForm = (
+      <div className="container-mobile">
+        <div className="input-mid inline pull-left">
+          <div className="button-input-rur">
+            <input
+              name="sum"
+              // type="number"
+              id="deposit"
+              value={state.deposit}
+              required="required"
+              autocomplete="off"
+              className="form-control circle-input"
+              onInput={this.onDepositInput}
+            />
+          </div>
+        </div>
+        <a id="depositLink3"
+           href={`/Payment?ammount=${state.deposit}&buyType=2`}
+           className="btn btn-lg btn-primary inline pull-left"
+        >Пополнить</a>
+      </div>
+    );
+
     //  на {state.deposit} РУБ
     const deposit = (
-      <div class="panel">
-        <div class="panel-heading">
-          <h3 id="depositMoney" class="mg-clear">Пополнить счет</h3>
+      <div className="panel">
+        <div className="panel-heading">
+          <h3 id="depositMoney" className="mg-clear">Пополнить счет</h3>
         </div>
-        <div class="panel-body">
+        <div className="panel-body">
           <form id="form-928" method="POST" action="https://money.yandex.ru/quickpay/confirm.xml" novalidate="novalidate">
-            <div class="form-group">
+            <div className="form-group">
               <label>Сумма</label>
               <input type="hidden" name="receiver" value="410013860652119" />
               <input type="hidden" name="formcomment" value="Пополнение счёта в online-tournaments.org" />
@@ -221,54 +244,45 @@ export default class Profile extends Component {
               <input type="hidden" id="sumAttribute" name="sum" value="4568.25" data-type="number" />
               <input type="hidden" name="comment" value="Платёж принят!" />
               <input type="hidden" name="paymentType" value="AC" />
-              <div className="button-sm">
-                <div className="button-input-rur">
-                  <input
-                    name="sum"
-                    id="deposit"
-                    value={state.deposit}
-                    required="required"
-                    autocomplete="off"
-                    className="form-control circle-input"
-                    onInput={this.onDepositInput}
-                  />
-                </div>
-              </div>
+              {depositForm}
             </div>
-            <a id="depositLink3"
-               href={`/Payment?ammount=${state.deposit}&buyType=2`}
-               className="btn btn-lg btn-primary"
-            >Пополнить</a>
           </form>
         </div>
       </div>
     );
 
+    const cashoutForm = (
+      <div className="container-mobile">
+        <div className="input-mid inline pull-left">
+          <div className="button-input-rur">
+            <input
+              name="sum"
+              // type="number"
+              min="500"
+              id="cashout"
+              value={state.cashout}
+              required="required"
+              autocomplete="off"
+              className="form-control circle-input"
+              onInput={this.onCashoutInput}
+            />
+          </div>
+        </div>
+        <button className="btn btn-lg btn-primary button">Вывести</button>
+      </div>
+    );
     const cashout = (
       <div>
-        <div class="panel">
-          <div class="panel-heading">
-            <h3 id="depositMoney" class="mg-clear">Вывод средств</h3>
+        <div className="panel">
+          <div className="panel-heading">
+            <h3 id="depositMoney" className="mg-clear">Вывод средств</h3>
           </div>
-          <div class="panel-body">
-            <div class="form-group">
-              <label class="full text-center">Минимальная сумма вывода - 500 рублей</label>
+          <div className="panel-body">
+            <div className="form-group">
+              <label className="full text-center">Минимальная сумма вывода - 500 рублей</label>
               <br />
-              <div className="button-sm">
-                <div className="button-input-rur">
-                  <input
-                    name="sum"
-                    id="cashout"
-                    value={state.cashout}
-                    required="required"
-                    autocomplete="off"
-                    className="form-control circle-input"
-                    onInput={this.onCashoutInput}
-                  />
-                </div>
-              </div>
+              {cashoutForm}
             </div>
-            <button class="btn btn-lg btn-primary button">Вывести</button>
           </div>
         </div>
         <h2 class="page">Вывод средств</h2>
