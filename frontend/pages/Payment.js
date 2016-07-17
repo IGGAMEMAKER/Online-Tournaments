f21daed37637d8cb60439acffa17cad19f8d0eea
IGGAMEMAKER <23i03g@mail.ru>
2016-07-17 22:26:48 +0300

    url in payment page works

--- frontend/pages/Payment.js
+++ frontend/pages/Payment.js
@@ -5,6 +5,9 @@ import { h, render } from 'preact';
 import Payment from '../components/Payments/index';
 
 const appElement: HTMLElement = document.getElementById('app');
+const url = location;
+const captured = /ammount=([^&]+)/.exec(url)[1]; // Value is in [1] ('384' in our case)
+const ammount = captured ? captured : 100;
 
 const elements = (
   <center>
