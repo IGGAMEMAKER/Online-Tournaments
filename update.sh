5a054f460a7f189c1cfb776fb07877f7d75f9347
IGGAMEMAKER <23i03g@mail.ru>
2016-07-23 15:43:47 +0300

    updated updater

--- update.sh
+++ update.sh
@@ -1,2 +1,15 @@
 git fetch --all
 git reset --hard origin/master
+cd frontend
+webpack
+sleep 1
+printf "5 seconds till start"
+sleep 1
+printf "4 seconds till start"
+sleep 1
+printf "3 seconds till start"
+sleep 1
+printf "2 seconds till start"
+sleep 1
+printf "1 seconds till start"
+sleep 1
