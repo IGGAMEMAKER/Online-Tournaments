26ed2270f718060063d8f2326f77ce786baf1ba8
IGGAMEMAKER <23i03g@mail.ru>
2016-08-26 21:39:21 +0300

    kill onde vk

--- routes/VK.js
+++ routes/VK.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var VKApi = require('node-vkapi');
+// var VKApi = require('node-vkapi');
 var middlewares = require('../middlewares');
 var respond = require('../middlewares/api-response');
 
@@ -12,28 +12,28 @@ var Users = require('../models/users');
 var API = require('../helpers/API');
 var MY_GROUP_ID = 1111871234;
 
-var VK = new VKApi({
+// var VK = new VKApi({
   // token: 'access_token'
-});
+// });
 
-router.get('/isJoinedGroup', middlewares.authenticated, respond((req) => {
-  return Users.profile(req.login)
-    .then(p => {
-      // console.log('isJoinedGroup: profile', p.social.id);
-      var userSocialID = p.social.id;
-      // if (!userSocialID) {
-      //   throw 'noVKUser' + req.login;
-      // }
-      // console.log('search for user', userSocialID);
-
-      var object = {
-        user_id: userSocialID,
-        group_id: MY_GROUP_ID
-      };
-
-      return VK.call('groups.isMember', object).then(r => !!r)
-    });
-}));
+// router.get('/isJoinedGroup', middlewares.authenticated, respond((req) => {
+//   return Users.profile(req.login)
+//     .then(p => {
+//       // console.log('isJoinedGroup: profile', p.social.id);
+//       var userSocialID = p.social.id;
+//       // if (!userSocialID) {
+//       //   throw 'noVKUser' + req.login;
+//       // }
+//       // console.log('search for user', userSocialID);
+//
+//       var object = {
+//         user_id: userSocialID,
+//         group_id: MY_GROUP_ID
+//       };
+//       return 1;
+//       return VK.call('groups.isMember', object).then(r => !!r)
+//     });
+// }));
 
 router.get('/', function (req, res) {
   res.render('Leagues');
